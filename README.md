563596e7a37dac0dd917756cb8e17d236cff0235
Soyokaze-42 <Soyokaze-42@users.noreply.github.com>
2022-01-27 11:03:22 -0600

    clarify assignment

--- README.md
+++ README.md
@@ -1,8 +1,30 @@
 **Bee Movie script analysis**
 
-We have four functions to write for a program that analyzes the words used
-in the Bee Movie. What is the most frequent word in the text file? What is 
-the probability you will find a given word in the script?
+Have you ever stopped to wonder, "How many times does is the word 'bee' spoken
+in Bee Movie?" or "What is the 50th most common word in the movie?"
 
-There are four functions with comments that need to be written in our program:
-bee_movie_analyzer.py
+If you have, you're in luck! We get to write four functions to analyze the
+words in the Bee Movie script.
+
+```
+nth_most_common_words(path, n)
+    The nth_most_common_words function takes the path to the script file and the 
+    word frequency rank and returns a list of words at that rank
+    return an empty string if the rank is below 1 or if the rank does not exist
+
+count_words(word_list)
+    The count_words function takes a word list and returns a dictionary with
+    the word as key and the value is the word count for that word
+
+get_words(path)
+    The get_words function takes a file path and returns a filtered list of
+    the words in the file. 
+    The words should all be lowercase
+    It filters out all the punctuation except contractions (they count as their own words).
+
+word_probability(path, word)
+    The word probability function takes a file path and a word and returns a float
+    that is a decimal value of the probability that any single word in the script
+    would be that word.
+    - If the word is not in the script, the probability is 0
+```
--- bee_movie_analyzer.py
+++ bee_movie_analyzer.py
@@ -1,38 +1,40 @@
 import re # you can use this module for a one line replacement of all punctuation
 
-# The nth_most_common_words function takes the path to the script file and the
-# word frequency rank and returns a list of words at that rank
-# return an empty string if the rank is below 1 or if the rank does not exist
 def nth_most_common_words(path, n):
-  words = get_words(path)
-  word_counts = count_words(words)
+    '''The nth_most_common_words function takes the path to the script file and the 
+    word frequency rank and returns a list of words at that rank
+    return an empty string if the rank is below 1 or if the rank does not exist'''
+    words = get_words(path)
+    word_counts = count_words(words)
  
   return
 
-# The count_words function takes a word list and returns a dictionary with
-# the word as key and the value is the word count for that word
+
 def count_words(word_list):
-  pass
+    '''The count_words function takes a word list and returns a dictionary with
+    the word as key and the value is the word count for that word'''
+    pass
 
-# The get_words function takes a file path and returns a filtered list of
-# the words in the file. 
-# The words should all be lowercase
-# It filters out all the punctuation except contractions (they count as their own words).
+
 def get_words(path):
-    words = []
-    for line in open(path): #loop through every line in the file. path should be a string
-      pass                  #of the file path to the script.
+    '''The get_words function takes a file path and returns a filtered list of
+    the words in the file. 
+    The words should all be lowercase
+    It filters out all the punctuation except contractions (they count as their own words).'''
+        words = []
+        for line in open(path): #loop through every line in the file. path should be a string
+            pass                   #of the file path to the script.
 
 
-# The word probability function takes a file path and a word and returns a float
-# that is a decimal value of the probability that any single word in the script
-# would be that word.
-# If the word is not in the script, the probability is 0
 def  word_probability(path, word):
-  words = get_words(path)
-  word_counts = count_words(words)
+    '''The word probability function takes a file path and a word and returns a float
+    that is a decimal value of the probability that any single word in the script
+    would be that word.
+    If the word is not in the script, the probability is 0'''
+    words = get_words(path)
+    word_counts = count_words(words)
 
-  return
+    return
 
 if __name__ == "__main__":
     #For extra practice, this would be a fun place to put a menu to access script stats
