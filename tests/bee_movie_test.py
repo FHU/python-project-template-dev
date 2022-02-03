82c8778a0d2e9589a1d589764eedbc97f2cc5a9d
Robbie Nichols <nichols.robbie@gmail.com>
2022-02-03 13:36:02 -0600

    update tests

--- tests/bee_movie_test.py
+++ tests/bee_movie_test.py
@@ -1,4 +1,4 @@
-from pytest import mark
+from pytest import mark, approx
 
 import bee_movie_analyzer
 
@@ -9,7 +9,7 @@ def test_get_words(test_word):
   assert test_word in words
 
 
-@mark.parametrize("test_word", ["jesus","Barry","fly.","great!","right?","so,"])
+@mark.parametrize("test_word", ["jesus","Barry","fly.","great!","right?","so,","\"you","-" ])
 def test_get_words_missing(test_word):
   words = bee_movie_analyzer.get_words("test.txt")
   
@@ -26,10 +26,10 @@ def test_count_words_nothing_extra():
   word_counts = bee_movie_analyzer.count_words(words)
   assert set(words) == set(word_counts.keys())
 
-@mark.parametrize("rank, word_list", [(1, ["you"]), (2, ["a","the"]), (3, ["i"]), (4, ["to"]), (21, ["do"]), (100, []), (0, [])])
+@mark.parametrize("rank, word_list", [(1, ["you"]), (2, ["the"]), (3, ["a"]), (4, ["i"]), (46, ['why', 'where', 'thinking']), (100, []), (0, [])])
 def test_nth_most_common_words(rank, word_list):
   assert bee_movie_analyzer.nth_most_common_words("test.txt", rank) == word_list
 
 @mark.parametrize("word, prob", [("i", 0.026108804894035394), ("buzz", 0.0014201441992571554), ("jesus", 0)])
 def test_word_probability(word, prob):
-  assert bee_movie_analyzer.word_probability("test.txt",word) == prob
+  assert bee_movie_analyzer.word_probability("test.txt",word) == approx(prob)
