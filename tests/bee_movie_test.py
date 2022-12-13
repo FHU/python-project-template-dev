ca4b1d3b677bffcc5a7bbaf3286d15bb703d2d8b
Robbie Nichols <nichols.robbie@gmail.com>
2022-12-13 16:24:42 -0600

    document tests

--- tests/bee_movie_test.py
+++ tests/bee_movie_test.py
@@ -5,6 +5,7 @@ import bee_movie_analyzer
 
 @mark.parametrize("test_word", ["buzz", "you", "barry", "you'll"])
 def test_get_words(test_word):
+    """Check that get_words returns a list that includes some of the correct words"""
     words = bee_movie_analyzer.get_words("test.txt")
 
     assert test_word in words
@@ -12,6 +13,7 @@ def test_get_words(test_word):
 
 @mark.parametrize("test_word", ["jesus", "Barry", "fly.", "great!", "right?", "so,", '"you', "-"])
 def test_get_words_missing(test_word):
+    """Check that get_words returns a list that does not include some words"""
     words = bee_movie_analyzer.get_words("test.txt")
 
     assert test_word not in words
@@ -19,6 +21,7 @@ def test_get_words_missing(test_word):
 
 @mark.parametrize("test_word, count", [("one", 1), ("two", 2), ("three", 3), ("four", 4)])
 def test_count_words(test_word, count):
+    """Check that count_words, givin a list of words, counts them appropriatly"""
     words = [
         "one",
         "two",
@@ -36,6 +39,7 @@ def test_count_words(test_word, count):
 
 
 def test_count_words_nothing_extra():
+    """Check that count_words does not include words not in the list passed to it"""
     words = [
         "one",
         "two",
@@ -65,6 +69,7 @@ def test_count_words_nothing_extra():
     ],
 )
 def test_nth_most_common_words(rank, word_list):
+    """Check that a few word ranks are accurate in nth_most_common_words"""
     assert bee_movie_analyzer.nth_most_common_words("test.txt", rank) == word_list
 
 
@@ -73,4 +78,5 @@ def test_nth_most_common_words(rank, word_list):
     [("i", 0.02610595303113053), ("buzz", 0.0014199890770070999), ("jesus", 0)],
 )
 def test_word_probability(word, prob):
+    """Check that a few word probabilities are accurate in word_probability"""
     assert bee_movie_analyzer.word_probability("test.txt", word) == approx(prob)
