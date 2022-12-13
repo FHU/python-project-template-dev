adb217279fa89fa6d500afaf2c0f08d9f6614ca6
Robbie Nichols <nichols.robbie@gmail.com>
2022-12-13 13:01:18 -0600

    add black to ci/cd and the solution (unblacked)

--- bee_movie_analyzer.py
+++ bee_movie_analyzer.py
@@ -1,57 +1,77 @@
-import re  # you can use this module for a one line replacement of all punctuation
-
+import re # you can use this module for a one line replacement of all punctuation
 
 def nth_most_common_words(path, n):
-    """The nth_most_common_words function takes the path to the script file and the
+    '''The nth_most_common_words function takes the path to the script file and the 
     word frequency rank and returns a list of words at that rank
-    return an empty string if the rank is below 1 or if the rank does not exist"""
+    return an empty string if the rank is below 1 or if the rank does not exist'''
     words = get_words(path)
     word_counts = count_words(words)
+ 
+    words_by_count = {}
+    for word, count in word_counts.items():
+        if count in words_by_count:
+            words_by_count[count].append(word)
+        else:
+            words_by_count[count] = [word]
 
-    return
+    ranks = list(words_by_count)
+    ranks.sort(reverse=True)
+
+    if n > len(ranks) or n == 0:
+        return []
+
+    return words_by_count[ranks[n-1]]
 
 
 def count_words(word_list):
-    """The count_words function takes a word list and returns a dictionary with
-    the word as key and the value is the word count for that word"""
-    pass
+    '''The count_words function takes a word list and returns a dictionary with
+    the word as key and the value is the word count for that word'''
+    word_counts = {}
+    for word in word_list:
+        if word in word_counts:
+            word_counts[word] += 1
+        else:
+            word_counts[word] = 1
+
+    return word_counts
 
 
 def get_words(path):
-    """The get_words function takes a file path and returns a filtered list of
-    the words in the file.
+    '''The get_words function takes a file path and returns a filtered list of
+    the words in the file. 
     The words should all be lowercase
-    It filters out all the punctuation except contractions (they count as their own words)."""
+    It filters out all the punctuation except contractions (they count as their own words).'''
     words = []
-    for line in open(
-        path
-    ):  # loop through every line in the file. path should be a string
-        pass  # of the file path to the script.
+    for line in open(path):           #loop through every line in the file. path should be a string
+        words.extend(re.sub(r'[,.?!"\-:]','',line).lower().split()) #of the file path to the script.
+    return words
+    
 
-
-def word_probability(path, word):
-    """The word probability function takes a file path and a word and returns a float
+def  word_probability(path, word):
+    '''The word probability function takes a file path and a word and returns a float
     that is a decimal value of the probability that any single word in the script
     would be that word.
-    If the word is not in the script, the probability is 0"""
+    If the word is not in the script, the probability is 0'''
     words = get_words(path)
     word_counts = count_words(words)
 
-    return
-
+    if word in words:
+        return word_counts[word] / len(words)
+    else:
+        return 0
 
 if __name__ == "__main__":
-    # For extra practice, this would be a fun place to put a menu to access script stats
-    # path = input("path: ")
-    # n = int(input("n: "))
-    # word = input("word: ")
+    #For extra practice, this would be a fun place to put a menu to access script stats
+    #path = input("path: ")
+    #n = int(input("n: "))
+    #word = input("word: ")
 
     path = "test.txt"
     n = 3
     word = "the"
-
-    words = nth_most_common_words(path, n)
-    print(words)
+    
+    for n in range(50):
+     print(nth_most_common_words(path, n))
 
     prob = word_probability(path, word)
     print(prob)
