f350801df00001048e682d0da4d2183382bc37e2
Robbie Nichols <nichols.robbie@gmail.com>
2022-12-15 11:38:35 -0600

    put htmlcov in _static

--- docs/conf.py
+++ docs/conf.py
@@ -56,6 +56,3 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
-
-# Add files generated from the coverage report
-html_extra_path = ["htmlcov"]
