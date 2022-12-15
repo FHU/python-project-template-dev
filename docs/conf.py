fbc0751cee26968da6de50b80b670dd2aad3a715
Robbie Nichols <nichols.robbie@gmail.com>
2022-12-15 13:19:42 -0600

    fix logo theme option

--- docs/conf.py
+++ docs/conf.py
@@ -52,7 +52,7 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 #
 html_theme = "sphinx_rtd_theme"
 
-html_theme_options = {"logo_file": "_static/doc_coverage_badge.svg"}
+html_theme_options = {"html_logo": "_static/doc_coverage_badge.svg"}
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
